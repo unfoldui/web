b9b198643803b6404f195cd296c5fec228fdcc81
Abhinav Sharma <as.abhinav@gmail.com>
2018-06-27 14:48:40 +0530

    Abhinav | Agenda new layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,7 +84,36 @@ $(document).ready(function ($) {
 
 	$('#attendance-form').click(() => {
 		modal.show();
-	})
+	});
+
+	(function () {
+		// define variables
+		var items = document.querySelectorAll(".agenda li");
+	
+		function isElementInViewport(el) {
+			var rect = el.getBoundingClientRect();
+			return (
+				rect.top >= 0 &&
+				rect.left >= 0 &&
+				rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+				rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+			);
+		}
+	
+		function callbackFunc() {
+			for (var i = 0; i < items.length; i++) {
+				if (isElementInViewport(items[i])) {
+					items[i].classList.add("in-view");
+				}
+			}
+		}
+	
+		// listen for events
+		window.addEventListener("load", callbackFunc);
+		window.addEventListener("resize", callbackFunc);
+		window.addEventListener("scroll", callbackFunc);
+	
+	})();
 
 });
 
