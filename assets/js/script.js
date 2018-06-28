439d8757b2040259cc7ef37b6cf7d43e0d5dee71
Ravi Sharma <rvisharma91@gmail.com>
2018-06-29 00:54:20 +0530

    Ravi - User Journey Completion * Thank you page creation * Redirect to Thank you page after submission * Minor styling changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,10 +67,6 @@ $(document).ready(function ($) {
 			}
 		});
 
-	$('#attendance-form').click(() => {
-		modal.show();
-	});
-
 	(function () {
 		// define variables
 		var items = document.querySelectorAll(".timeline .entry--left, .timeline .entry--right ");
@@ -109,34 +105,3 @@ $(document).ready(function ($) {
 function Resize() {
 	$(window).trigger('resize');
 }
-
-const modal = picoModal({
-	content: document.getElementById('registration-form'),
-	overlayStyles: function (styles) { styles.opacity = 0; },
-	closeStyles: {
-		width:'30px',
-		height:'30px',
-		position:'absolute',
-		top:'5px',
-		right: '5px',
-		borderRadius: '50%',
-		cursor: 'pointer',
-		boxShadow: '0px 3px 11px 1px rgba(108, 117, 125, 0.41)'
-	},
-	modalStyles: function (styles) {
-		styles.opacity = 0,
-		styles.padding = 0,
-		styles.marginTop = '40px',
-		styles.width = (window.innerWidth <= 600) ? '90vw' : '700px'
-	}
-}).afterShow(function (modal) {
-	$(modal.overlayElem()).animate({ opacity: 0.7 });
-	$(modal.modalElem()).animate({ opacity: 1 });
-}).beforeClose(function (modal, event) {
-	event.preventDefault();
-	$(modal.overlayElem()).add(modal.modalElem())
-		.animate(
-			{ opacity: 0 },
-			{ complete: modal.forceClose }
-		);
-});
